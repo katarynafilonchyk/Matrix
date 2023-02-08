0090df5ccea8341bbcdc75457418c6df7f01e8f6
Катарина Филончик <119134012+katarynafilonchyk@users.noreply.github.com>
2023-02-08 15:02:33 +0300

    task towelSort

--- towel-sort/src/index.js
+++ towel-sort/src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  for(let i = 0; i < matrix.length; i++) {
+    for(let j = 0; j < matrix[i].length; j++) {
+      let Idx = i % 2 === 0
+      ? j
+      : matrix[i].length - j - 1 ;
+      return [matrix[i][Idx]];
+    }
+  }
 }
